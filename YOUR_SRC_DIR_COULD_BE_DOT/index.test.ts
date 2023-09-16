44e7ec5d17e07c84571c353f711a809bca8d0384
Konrad Markus <konker@iki.fi>
2023-09-16 20:19:59 +0300

    Add code with static asset

--- YOUR_SRC_DIR_COULD_BE_DOT/index.test.ts
+++ YOUR_SRC_DIR_COULD_BE_DOT/index.test.ts
@@ -1,7 +1,15 @@
 import * as unit from './index';
 
 describe('index', () => {
-  it('should work as expected', () => {
-    expect(unit.f(1, 2)).toEqual(3);
+  describe('f', () => {
+    it('should work as expected', () => {
+      expect(unit.f(1, 2)).toEqual(3);
+    });
+  });
+
+  describe('g', () => {
+    it('should work as expected', async () => {
+      expect(await unit.g()).toEqual(`body {\n  background-color: #0ff;\n}\n`);
+    });
   });
 });
--- YOUR_SRC_DIR_COULD_BE_DOT/index.ts
+++ YOUR_SRC_DIR_COULD_BE_DOT/index.ts
@@ -1,3 +1,10 @@
+import * as fs from 'node:fs';
+import path from 'node:path';
+
 export function f(a: number, b: number): number {
   return a + b;
+}
+
+export async function g(): Promise<string> {
+  return fs.promises.readFile(path.join(__dirname, 'static', 'styles.css'), 'utf8');
 }

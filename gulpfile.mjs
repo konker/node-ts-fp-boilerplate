a123e0834dc3a11e29025b3df0aec2df908907d1
Konrad Markus <konker@iki.fi>
2023-09-17 00:18:05 +0300

    Add gulp for static file copying

---
+++ gulpfile.mjs
@@ -0,0 +1,10 @@
+import gulp from 'gulp';
+import packageJson from './package.json' assert { type: 'json' };
+
+gulp.task('copy-static', async () => {
+  return gulp
+    .src(packageJson.staticFiles, {
+      base: 'YOUR_SRC_DIR_COULD_BE_DOT',
+    })
+    .pipe(gulp.dest('dist'));
+});
